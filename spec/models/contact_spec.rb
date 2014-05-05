363b62583cc959d6847ece8bc482275a1b134476
dtan4 <dtanshi45@gmail.com>
2014-05-05 23:05:42 +0900

    Use FactoryGirl

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,46 +2,34 @@ require "spec_helper"
 
 describe Contact do
   it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-                          firstname: "Aaron",
-                          lastname: "Summer",
-                          email: "tester@example.com"
-                         )
-    expect(contact).to be_valid
+    expect(build(:contact)).to be_valid
   end
 
   it "is invalid without a firstname" do
-    expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+    contact = build(:contact, firstname: nil)
+    expect(contact).to have(1).errors_on(:firstname)
   end
 
   it "is invalid withour a lastname" do
-    expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+    contact = build(:contact, lastname: nil)
+    expect(contact).to have(1).errors_on(:lastname)
   end
 
   it "is invalid without an email address" do
-    expect(Contact.new(email: nil)).to have(1).errors_on(:email)
+    contact = build(:contact, email: nil)
+    expect(contact).to have(1).errors_on(:email)
   end
 
   it "is invalid with a duplicate email address" do
-    Contact.create(
-                   firstname: "Joe",
-                   lastname: "Tester",
-                   email: "tester@example.com"
-                  )
-    contact = Contact.create(
-                             firstname: "Jane",
-                             lastname: "Tester",
-                             email: "tester@example.com"
-                            )
+    create(:contact,
+                       firstname: "Joe", lastname: "Tester", email: "tester@example.com")
+    contact = build(:contact,
+                                firstname: "Jane", lastname: "Tester", email: "tester@example.com")
     expect(contact).to have(1).errors_on(:email)
   end
 
   it "returns a contact's full name as a string" do
-    contact = Contact.new(
-                          firstname: "John",
-                          lastname: "Doe",
-                          email: "johndoe@example.com"
-                         )
+    contact = build(:contact, firstname: "John", lastname: "Doe")
     expect(contact.name).to eq "John Doe"
   end
 
