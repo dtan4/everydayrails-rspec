7b37271aecbe30967ce47ac2d6c8ffa64ac9cfaf
dtan4 <dtanshi45@gmail.com>
2014-05-05 22:39:56 +0900

    Refactor specs

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -45,41 +45,35 @@ describe Contact do
     expect(contact.name).to eq "John Doe"
   end
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-                           firstname: "John",
-                           lastname: "Smith",
-                           email: "jsmith@example.com"
-                          )
-    jones = Contact.create(
-                           firstname: "Tim",
-                           lastname: "Jones",
-                           email: "tjones@example.com"
-                          )
-    johnson = Contact.create(
+  describe "filter last name by letter" do
+    before do
+      @smith = Contact.create(
                              firstname: "John",
-                             lastname: "Johnson",
-                             email: "jjohnson@example.com"
+                             lastname: "Smith",
+                             email: "jsmith@example.com"
                             )
-    expect(Contact.by_letter("J")).to eq [johnson, jones]
-  end
+      @jones = Contact.create(
+                             firstname: "Tim",
+                             lastname: "Jones",
+                             email: "tjones@example.com"
+                            )
+      @johnson = Contact.create(
+                               firstname: "John",
+                               lastname: "Johnson",
+                               email: "jjohnson@example.com"
+                              )
+    end
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-                           firstname: "John",
-                           lastname: "Smith",
-                           email: "jsmith@example.com"
-                          )
-    jones = Contact.create(
-                           firstname: "Tim",
-                           lastname: "Jones",
-                           email: "tjones@example.com"
-                          )
-    johnson = Contact.create(
-                             firstname: "John",
-                             lastname: "Johnson",
-                             email: "jjohnson@example.com"
-                            )
-    expect(Contact.by_letter("J")).not_to include smith
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
+    end
+
+    context "non-mathing letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).not_to include @smith
+      end
+    end
   end
 end
