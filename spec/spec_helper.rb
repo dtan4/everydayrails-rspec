12ee141ad7ac8d27435473e0a1bd3428cbe4eb81
Daisuke Fujita <dtanshi45@gmail.com>
2015-02-06 01:21:31 +0900

    Remove

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ Coveralls.wear!("rails")
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 require "shoulda/matchers"
 
