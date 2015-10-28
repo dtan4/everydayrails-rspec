93a4eafaaeb771633e50a1f1285b55d78eb86963
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-28 16:31:03 +0900

    Use shoulda-matchers 3.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -64,3 +64,10 @@ RSpec.configure do |config|
 
   config.include LoginMacros
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
