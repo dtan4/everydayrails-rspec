f906cbed62e497a6e1931ff5db0e9e0b9695a0fd
Daisuke Fujita <dtanshi45@gmail.com>
2021-02-23 22:41:11 +0900

    Use headless Chrome driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,12 +14,12 @@ require "database_cleaner"
 require "capybara/rspec"
 require "selenium-webdriver"
 
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app,
     browser: :chrome,
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      chrome_options: {
-        args: %w(headless no-sandbox disable-gpu window-size=1680,1050),
+      chromeOptions: {
+        args: %w(headless no-sandbox disable-gpu disable-dev-shm-usage),
       },
     )
   )
