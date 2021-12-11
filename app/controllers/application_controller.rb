91bf4f3b22c641c16d75a40b0366954b70c4174d
Daisuke Fujita <dtanshi45@gmail.com>
2021-12-12 02:26:19 +0900

    Correct more findings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def administrator?
-    current_user && current_user.admin?
+    current_user&.admin?
   end
 
   helper_method :administrator?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
