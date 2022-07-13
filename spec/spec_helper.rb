7403e59b19d62974acdfc60e0f9768d759f5e225
Daisuke Fujita <dtanshi45@gmail.com>
2022-07-13 21:59:27 +0900

    Use remote Chrome container instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,7 +78,18 @@ RSpec.configure do |config|
   config.include LoginMacros
 
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    if ENV['SELENIUM_DRIVER_URL']
+      driven_by :selenium, using: :headless_chrome, options: {
+        browser: :remote,
+        url: ENV.fetch('SELENIUM_DRIVER_URL'),
+        desired_capabilities: :chrome
+      }
+      Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+      Capybara.server_port = 3000
+      Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
 end
 
