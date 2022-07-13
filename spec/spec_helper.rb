71655acbbb411cb4b2f1852004e13d2e37c4a699
Daisuke Fujita <dtanshi45@gmail.com>
2022-07-13 13:27:48 +0000

    Update capybara and selenium-webdriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -68,7 +68,7 @@ RSpec.configure do |config|
       driven_by :selenium, using: :headless_chrome, options: {
         browser: :remote,
         url: ENV.fetch('SELENIUM_DRIVER_URL'),
-        desired_capabilities: :chrome
+        capabilities: :chrome
       }
       Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
       Capybara.server_port = 3000
